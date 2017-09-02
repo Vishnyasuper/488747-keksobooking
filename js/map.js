775a35b0d20bb7e116b2c7b5dc36583e2698eae0
Yuliya <ylobukhova@gmail.com>
2017-09-02 14:21:49 +0300

    Создание массива объявлений

--- js/map.js
+++ js/map.js
@@ -1,5 +1,6 @@
-//Файл map.js
-'use strict'
+// Файл map.js
+'use strict';
+
 var ADVERT_AVATAR = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'];
 
 var ADVERT_TITLE = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
